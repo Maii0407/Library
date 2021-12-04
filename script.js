33e702f09f9ff10e88effdca59fbd9785025640c
Akmal Izuddin <izuddin0407@gmail.com>
2021-12-05 06:44:28 +0800

    Finished making so user can push what they input in form to the library array and the HTML table. Next step is making so user can toggle between read and not read on books in the table. And making a button so user can remove objects from the library array and the table.

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ let table = document.getElementById('table');
 let myLibrary = [];
 
 const createBook = document.getElementById('createBookBtn');
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const readInput = document.getElementById('readInput');
 
 //default book list in library array
-const dragonBall = new Book('Dragon Ball', 'Akira Toriyama', '42 Vols', 'Read');
-const onePiece = new Book('One Piece', 'Eiichiro Oda', '101 Vols - ongoing', 'Read');
-const kingInBlack = new Book('King In Black', 'Donny Cates', '5 Vols', 'Read');
+const dragonBall = new Book('Dragon Ball', 'Akira Toriyama', 'Read');
+const onePiece = new Book('One Piece', 'Eiichiro Oda', 'Read');
+const kingInBlack = new Book('King In Black', 'Donny Cates', 'Read');
 myLibrary.push(dragonBall, onePiece, kingInBlack);
 
 //eventlisteners
@@ -19,29 +22,33 @@ for(let obj of myLibrary){
     let row = table.insertRow();
     row.insertCell().textContent = obj.title;
     row.insertCell().textContent = obj.author;
-    row.insertCell().textContent = obj.pages;
     row.insertCell().textContent = obj.read;
 }
 
 //functions
-function Book(title, author, pages, read){
+function Book(title, author, read){
  this.title = title;
  this.author = author;
- this.pages = pages;
  this.read = read;
  this.info = function(){
-     return title + ' by ' +  author + ', ' + pages + ', ' + read;
+     return title + ' by ' +  author + ', ' + read;
  }
 }
 
 function addBookToLibrary(newBook){
- let titleInput = document.getElementById('titleInput');
- let authorInput = document.getElementById('authorInput');
- let pagesInput = document.getElementById('pagesInput');
- let readInput = document.getElementById('readInput');
-
- newBook = new Book(titleInput.innerText, authorInput.innerText, pagesInput.innerText, readInput.innerText);
+ newBook = new Book(titleInput.value, authorInput.value, readInput.value);
  myLibrary.push(newBook);
+
+ let row = table.insertRow();
+ row.insertCell().textContent = titleInput.value;
+ row.insertCell().textContent = authorInput.value;
+ row.insertCell().textContent = readInput.value;
+
+ closeForm();
+ titleInput.value = '';
+ authorInput.value = '';
+ readInput.value = '';
+
 }
 
 function openForm(){
