9fcd290039db6646cda10497800336a403ef0412
Akmal Izuddin <izuddin0407@gmail.com>
2021-12-06 14:11:46 +0800

    finally completed the library project lol. thank fuuucking god. just need some final styling

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ const createBook = document.getElementById('createBookBtn');
 const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
 const readInput = document.getElementById('readInput');
-const readBtn = document.getElementById('readBtn');
 
 //default book list in library array
 const dragonBall = new Book('Dragon Ball', 'Akira Toriyama', 'Read');
-const onePiece = new Book('One Piece', 'Eiichiro Oda', 'Read');
 const kingInBlack = new Book('King In Black', 'Donny Cates', 'Read');
-myLibrary.push(dragonBall, onePiece, kingInBlack);
+myLibrary.push(dragonBall, kingInBlack);
 
 //eventlisteners
 createBook.addEventListener('click', addBookToLibrary);
@@ -23,7 +21,7 @@ for(let obj of myLibrary){
     let row = table.insertRow();
     row.insertCell().textContent = obj.title;
     row.insertCell().textContent = obj.author;
-    row.insertCell().innerHTML = `<button id='readBtn'>${obj.read}</button>`
+    row.insertCell().innerHTML = `<button class='readBtn' onclick="change(this)">${obj.read}</button> <button class="deleteBtn" onclick='removeRow(this)'>X</button>`
 }
 
 //functions
@@ -40,12 +38,25 @@ function addBookToLibrary(newBook){
  let row = table.insertRow();
  row.insertCell().textContent = titleInput.value.toUpperCase();
  row.insertCell().textContent = authorInput.value.toUpperCase();
- row.insertCell().innerHTML = `<button id='readBtn'>${readInput.value.toUpperCase()}</button>`;
+ row.insertCell().innerHTML = `<button class='readBtn' onclick='change(this)'>${readInput.value.toUpperCase()}</button> <button class="deleteBtn" onclick='removeRow(this)'>X</button>`;
 
  closeForm();
  titleInput.value = '';
  authorInput.value = '';
  readInput.value = '';
+}
+
+function removeRow(button){
+    let r = button.parentNode.parentNode.rowIndex;
+    document.getElementById('table').deleteRow(r);
+}
+
+function change(button){
+    if(button.innerHTML === 'READ'){
+        button.innerHTML = 'NOT READ';
+    } else{
+        button.innerHTML = 'READ';
+    }
 }
 
 function openForm(){
